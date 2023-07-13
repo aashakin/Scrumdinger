f58310d9f251a63a759df9025b5311139c20aedb
Aleksandr Shakin <alexandr.shakin@gmail.com>
2023-07-13 17:01:51 +0400

    5. Detail Edit View Prototype

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,60 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by Aleksandr on 13.07.2023.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    
+    @State private var scrum = DailyScrum.empty
+    @State private var newAttendeeName = ""
+    
+    var body: some View {
+        Form {
+            Section(header: Text("Meeting Info")) {
+                TextField("Title", text: $scrum.title)
+                HStack {
+                    Slider(value: $scrum.lengthInMinutesAsDouble, in: 5...30, step: 1) {
+                        Text("Length")
+                    }
+                    .accessibilityValue("\(scrum.lengthInMinutes) minutes")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes)")
+                        .accessibilityHidden(true)
+                }
+            }
+            
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete { indicies in
+                    scrum.attendees.remove(atOffsets: indicies)
+                }
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            scrum.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    } label: {
+                        Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
+                }
+            }
+        }
+    }
+}
+
+struct DetailEditView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailEditView()
+    }
+}
